56f16159308ccea818a6af1f8c502fe7c67e3e48
Bryant1356 <bryantv0.2.0@gmail.com>
2023-09-01 14:33:59 +0700

    frontend first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/features/authSlice.js
@@ -0,0 +1,82 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ""
+}
+
+export const Login = createAsyncThunk("user/Login", async (user, thunkAPI) => {
+    try {
+        const response = await axios.post('http://localhost:5000/login', {
+            email: user.email,
+            password: user.password
+        });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            const message = error.response.data.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
+export const Profile = createAsyncThunk("user/Profile", async (_, thunkAPI) => {
+    try {
+        const response = await axios.get('http://localhost:5000/me');
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            const message = error.response.data.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
+export const Logout = createAsyncThunk("user/Logout", async () => {
+    await axios.delete('http://localhost:5000/logout');
+});
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        builder.addCase(LoginUser.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(LoginUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        });
+        builder.addCase(LoginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        });
+
+        // Get User Login
+        builder.addCase(Profile.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(Profile.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        });
+        builder.addCase(Profile.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        });
+    }
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
