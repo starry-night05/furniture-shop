b4f1b81747fc5f96c7e55129cf65255f051c574e
Bryant1356 <bryantv0.2.0@gmail.com>
2023-09-16 13:31:18 +0700

    add Product

--- frontend/src/views/Components/admin/NewProduct.jsx
+++ frontend/src/views/Components/admin/NewProduct.jsx
@@ -9,18 +9,43 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Unstable_Grid2'
 import FormGroup from '@mui/material/FormControl'
+import ImageList from '@mui/material/ImageList'
 import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import Stack from '@mui/material/Stack'
 
 // icon.component
 
 const NewProduct = () => {
+    const [categories, setCategories] = useState([]);
     const [file, setFile] = useState('');
     const [preview, setPreview] = useState('');
+
+    // Mendapatkan semua data produk
+    useEffect(() => {
+        getCategories();
+    }, []);
+
+    const getCategories = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/categories');
+            setCategories(response.data);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    };
+
     const loadImage = (e) => {
         const image = e.target.files[0];
         setFile(image);
         setPreview(URL.createObjectURL(image));
     }
+
+    const defaultProps = {
+        options: categories,
+        getOptionLabel: (option) => option.category,
+    };
+
     return (
         <Sidebar>
             <Box component="main" sx={{ flexGrow: 1, p: 3, mt: { xs: 13, md: 15 }, ml: { xs: 0, md: 2 } }}>
@@ -31,9 +56,16 @@ const NewProduct = () => {
                                 <Grid md={6} xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                                     <CardContent>
                                         {/* <p style={{ background: 'red', color: '#fff', fontWeight: 'bold' }}>{msg}</p> */}
-                                        <FormGroup sx={{ mb: '1.3rem' }}>
+                                        <FormGroup sx={{ mb: { xs: '-1rem', md: '1.3rem' } }}>
                                             {preview ? (
-                                                <img src={preview} alt="preview" style={{ width: '320px', height: '320px', marginTop: '1rem' }} />
+                                                <ImageList sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                                                        <img src={preview} alt="preview" loading="lazy" style={{ width: '320px', height: '320px', marginTop: '1rem' }} />
+                                                    </Box>
+                                                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                                                        <img src={preview} alt="preview" loading="lazy" style={{ width: '150px', height: '150px', marginTop: '1rem' }} />
+                                                    </Box>
+                                                </ImageList>
                                             ) : (
                                                 <Typography sx={{
                                                     fontFamily: 'Poppins',
@@ -67,8 +99,37 @@ const NewProduct = () => {
                                 <Grid md={6} xs={12}>
                                     <CardContent>
                                         <FormGroup>
-                                            <Typography variant="body2" color="initial">Nama Produk</Typography>
-                                            <input type="text" name="product_name" id="product_name" placeholder='Nama produk...' style={{ width: '235px', padding: '5px' }} />
+                                            <Typography variant="body2" color="initial" sx={{
+                                                fontFamily: 'Poppins'
+                                            }}>
+                                                Nama Produk :
+                                            </Typography>
+                                            <TextField type="text" name="product_name" id="product_name" placeholder='Nama produk...' size='small' sx={{ width: { md: '350px', xs: '235px' } }} />
+                                        </FormGroup>
+                                        <FormGroup sx={{ mt: '1rem' }}>
+                                            <Typography variant="body2" color="initial" sx={{
+                                                fontFamily: 'Poppins'
+                                            }}>
+                                                Kategori :
+                                            </Typography>
+                                            <Stack spacing={1} sx={{ width: { xs: '235px', md: '350px' } }}>
+                                                <Autocomplete
+                                                    {...defaultProps}
+                                                    id="clear-on-escape"
+                                                    clearOnEscape
+                                                    renderInput={(params) => (
+                                                        <TextField {...params} value={categories.id} placeholder='Pilih kategori...' variant="standard" name='categoryId' />
+                                                    )}
+                                                />
+                                            </Stack>
+                                        </FormGroup>
+                                        <FormGroup sx={{ mt: '1rem' }}>
+                                            <Typography variant="body2" color="initial" sx={{
+                                                fontFamily: 'Poppins'
+                                            }}>
+                                                Harga :
+                                            </Typography>
+                                            <TextField type="number" name="price" id="harga" placeholder='Rp...' size='small' sx={{ width: { md: '350px', xs: '235px' } }} />
                                         </FormGroup>
                                     </CardContent>
                                 </Grid>
@@ -80,5 +141,4 @@ const NewProduct = () => {
         </Sidebar>
     )
 }
-
 export default NewProduct
