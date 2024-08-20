d7853d6506e5a7fe37e6f57781e99e29f5c8b09e
rizki-febriyanto <rizkifebriyanto1239718@gmail.com>
2024-08-20 20:34:12 +0700

    update comment

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -31,6 +31,85 @@ export const getUserById = async (req, res) => {
     }
 }
 
+// edit profile
+export const editProfile = async (req, res) => {
+    const user = await Users.findOne({
+        where: {
+            id: req.userId
+        }
+    });
+    if (!user) return res.status(404).json({ msg: 'User tidak ditemukan' });
+
+    const { firstname, lastname, password, confPassword, mail, phone, address } = req.body;
+    const file = req.files.file;
+    const size = file.data.length;
+    const ext = path.extname(file.name);
+    const uniqueIdentifier = Date.now();
+    const fileName = `${file.md5}_${uniqueIdentifier}${ext}`;
+    const url = `${req.protocol}://${req.get("host")}/images/profile/${fileName}`;
+    const allowedType = ['.jpeg', '.jpg', '.png'];
+    if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: "Gambar harus berekstensi .jpeg, .jpg, dan .png" });
+
+    if (size > 5000000) return res.status(422).json({ msg: "Ukuran file maksimal 5mb" });
+
+    let email = user.email;
+    let tlp = user.tlp;
+    let hashPassword = user.password;
+    if (password) {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$!&*]).{8,}$/;
+        if (!passwordRegex.test(password)) {
+            return res.status(400).json({ msg: 'Password setidaknya mengandung satu huruf kapital, angka, dan simbol (@,#,$,!,&,*), serta memiliki panjang minimal 8 karakter' });
+        }
+        if (password !== confPassword) {
+            return res.status(400).json({ msg: 'Password dan konfirmasi password tidak sama' });
+        }
+        hashPassword = await argon2.hash(password);
+    }
+    if (mail || phone) {
+        const emailUser = await Users.findOne({
+            attributes: ['email', 'tlp'],
+            where: {
+                id: { [Op.not]: user.id },
+                [Op.or]: [{ email: mail }, { tlp: phone }]
+            }
+        });
+        if (emailUser) {
+            if (mail === emailUser.email) {
+                return res.status(422).json({ msg: 'Email sudah digunakan, gunakan email lain!' });
+            }
+            if (phone === emailUser.tlp) {
+                return res.status(422).json({ msg: 'No. Hp sudah digunakan, gunakan No. Hp lain!' });
+            }
+        }
+        if (mail) email = mail;
+        if (phone) tlp = phone;
+    }
+
+    file.mv(`./public/images/profile/${fileName}`, async (err) => {
+        if (err) return res.status(500).json({ msg: err.message });
+        try {
+            await Users.update({
+                firstname: firstname,
+                lastname: lastname,
+                password: hashPassword,
+                image: fileName,
+                url: url,
+                email: email,
+                tlp: tlp,
+                address: address
+            },
+                {
+                    where: {
+                        id: user.id
+                    }
+                });
+            res.status(200).json({ msg: 'Update berhasil' });
+        } catch (error) {
+            res.status(400).json({ msg: error.message });
+        }
+    });
+}
+
 // Registrasi akun
 export const regUser = async (req, res) => {
     const { firstname, lastname, password, confPassword, email, tlp } = req.body; // request parameters
@@ -90,85 +169,6 @@ export const createUser = async (req, res) => {
     } catch (error) {
         res.status(400).json({ msg: error.message });
     }
-}
-
-// edit profile
-export const editProfile = async (req, res) => {
-    const user = await Users.findOne({
-        where: {
-            id: req.userId
-        }
-    });
-    if (!user) return res.status(404).json({ msg: 'User tidak ditemukan' });
-    const { firstname, lastname, password, confPassword, mail, phone, address } = req.body;
-    const file = req.files.file;
-    const size = file.data.length;
-    const ext = path.extname(file.name);
-    const uniqueIdentifier = Date.now();
-    const fileName = `${file.md5}_${uniqueIdentifier}${ext}`;
-    const url = `${req.protocol}://${req.get("host")}/images/profile/${fileName}`;
-    const allowedType = ['.jpeg', '.jpg', '.png'];
-
-    if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: "Gambar harus berekstensi .jpeg, .jpg, dan .png" }); // Jika ekstensi gambar tidak sesuai
-
-    if (size > 5000000) return res.status(422).json({ msg: "Ukuran file maksimal 5mb" });
-
-    let email;
-    let tlp;
-    let hashPassword;
-    if (password === "" || password === null || mail === "" || mail === null || phone === "" || phone === null) { // Jika pengguna tidak melakukan update
-        hashPassword = user.password
-        email = user.email
-        tlp = user.tlp
-    } else { // Jika pengguna melakukan update data
-        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$!&*]).{8,}$/;
-        if (!passwordRegex.test(password)) {
-            return res.status(400).json({ msg: 'Password setidaknya mengandung satu huruf kapital, angka, dan simbol (@,#,$,!,&,*), serta memiliki panjang minimal 8 karakter' });
-        }
-        hashPassword = await argon2.hash(password)
-        const emailUser = await Users.findOne({
-            attributes: ['email', 'tlp'],
-            where: {
-                id: { [Op.not]: user.id },
-                [Op.or]: [{ email: req.body.mail }, { tlp: req.body.phone }]
-            }
-        });
-        if (emailUser) {
-            if (mail === emailUser.email) {
-                return res.status(422).json({ msg: 'Email sudah digunakan, gunakan email lain!' });
-            }
-            if (phone === emailUser.tlp) {
-                return res.status(422).json({ msg: 'No. Hp sudah digunakan, gunakan No. Hp lain!' });
-            }
-        }
-        email = mail;
-        tlp = phone;
-    }
-    if (password !== confPassword) return res.status(400).json({ msg: 'password dan konfirmasi password tidak sama' });
-    file.mv(`./public/images/profile/${fileName}`, async (err) => {
-        if (err) return res.status(500).json({ msg: err.message });
-        try {
-            await Users.update({
-                firstname: firstname,
-                lastname: lastname,
-                password: hashPassword,
-                image: fileName,
-                url: url,
-                email: email,
-                tlp: tlp,
-                address: address
-            },
-                {
-                    where: {
-                        id: user.id
-                    }
-                }
-            );
-            res.status(200).json({ msg: 'Update berhasil' });
-        } catch (error) {
-            res.status(400).json({ msg: error.message });
-        }
-    });
 }
 
 // edit user
