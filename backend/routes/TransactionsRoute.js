2ea9a9605c497227dac8be44ac473993c602f204
Mofals <60801177+Mofals@users.noreply.github.com>
2024-08-20 17:47:17 +0700

    Update TransactionsRoute.js

--- backend/routes/TransactionsRoute.js
+++ backend/routes/TransactionsRoute.js
@@ -15,12 +15,15 @@ import { verifyUser, adminOnly } from '../middleware/Auth.js';
 const router = express.Router();
 
 // Users
-router.post('/confirm', verifyUser, confirmOrder);
 router.get('/checkoutList', verifyUser, checkoutList);
+
+router.post('/confirm', verifyUser, confirmOrder);
 router.patch('/cancelOrder/:id', verifyUser, cancelOrder);
 router.patch('/receiveOrder/:id', verifyUser, receiveOrder);
+
 // Admin only
 router.get('/Transactions', adminOnly, verifyUser, getAllTransactions);
+
 router.patch('/confirm/:id', adminOnly, verifyUser, confirm);
 router.patch('/cancel/:id', adminOnly, verifyUser, cancel);
 router.patch('/shipping/:id', adminOnly, verifyUser, shipping);
