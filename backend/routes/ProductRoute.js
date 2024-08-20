d3e91186f15aea05a44b6891479ad1ad8bbbc3ee
Mofals <60801177+Mofals@users.noreply.github.com>
2024-08-20 17:41:11 +0700

    Update ProductRoute.js

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -12,10 +12,12 @@ import { verifyUser, adminOnly } from '../middleware/Auth.js';
 const router = express.Router();
 
 router.get('/products', getProducts);
+
 router.get('/product/:id', getProductById);
+router.post('/product', verifyUser, adminOnly, createProduct);
+router.patch('/product/:id', adminOnly, updateProduct);
+router.delete('/product/:id', adminOnly, deleteProduct);
+
 router.get('/products-category/:id', verifyUser, getProductBycategory);
-router.post('/addProduct', verifyUser, adminOnly, createProduct);
-router.patch('/updateProduct/:id', adminOnly, updateProduct);
-router.delete('/removeProduct/:id', adminOnly, deleteProduct);
 
 export default router;
